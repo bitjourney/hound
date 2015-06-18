8639c7689f1d085b7484bd49426c00d971dcf99d
dex1t <dex1t@degoo.org>
2015-06-18 18:26:07 +0900

    Don't use basic auth on application

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -22,10 +22,6 @@ Resque::Failure.backend = Resque::Failure::Multiple
 
 Resque::Failure::Sentry.logger = "resque"
 
-Resque::Server.use(Rack::Auth::Basic) do |user, password|
-  password == ENV['RESQUE_ADMIN_PASSWORD']
-end
-
 ApplicationJob.timeout = ENV.fetch("RESQUE_JOB_TIMEOUT", 120).to_i
 Retryable.retry_delay = ENV.fetch("RESQUE_RETRY_DELAY", 30).to_i
 Retryable.retry_attempts = ENV.fetch("RESQUE_RETRY_ATTEMPTS", 10).to_i
